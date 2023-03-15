fed7d01226b6ef21d70588163a1485f17d8da2c3
Vihan Jayaraman <vjayaraman@cs.ucsb.edu>
2023-03-14 22:44:00 -0700

    added movies.cpp and suppressed warning comp messages in Makefile

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char** argv){
 
   if (argc == 2){
     for (auto it = movies.begin(); it != movies.end(); ++it) {
-        cout << (*it).name << ": " << (*it).rating << endl;
+        cout << (*it).name << ", " << (*it).rating << endl;
     }
     return 0;
   }
@@ -89,7 +89,7 @@ int main(int argc, char** argv){
             highestRatedMovies << "Best movie with prefix " << argv[i] << " is: " << prefixList.top().name << " with rating " << std::fixed << std::setprecision(1) << prefixList.top().rating << endl;
         
             while (!prefixList.empty()) {
-                cout << prefixList.top().name << ": " << std::fixed << std::setprecision(1) << prefixList.top().rating << endl;
+                cout << prefixList.top().name << ", " << std::fixed << std::setprecision(1) << prefixList.top().rating << endl;
                 prefixList.pop();
             }
         }
